800a40388ed9fa3da77a876ff10a5b535d1b4e9a
Jonathan Chan <johnkchan252@gmail.com>
2019-06-10 03:07:02 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -17,6 +17,6 @@ function nameToAttributes(drivers) {
 
 function attributesToPhrase(drivers) {
   return drivers.map(function(driver) {
-    
+    return driver.name + " is from " + driver.hometown;
   });
 }
