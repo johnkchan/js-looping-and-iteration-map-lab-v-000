073eea835fe196b027f9f03bf712a51fd59bf904
Jonathan Chan <johnkchan252@gmail.com>
2019-06-10 03:04:28 +0000

    Automatically backed up by Learn

--- index.js
+++ index.js
@@ -8,7 +8,7 @@ function lowerCaseDrivers(drivers) {
 }
 
 function nameToAttributes(drivers) {
-  drivers.map(function(driver) {
+  return drivers.map(function(driver) {
     return Object.assign({}, driver, {
       firstName: driver.slice(0, driver.search(" ")),
       lastName: "test"
